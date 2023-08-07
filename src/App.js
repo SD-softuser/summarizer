5e14a9d5b5da28d8d009cfaf04c32b3a8a004b36
SD-softuser <sayogdmahajan@gmail.com>
2023-08-08 02:50:29 +0530

    nav bar created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import github from "./assets/github.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="w-full bg-[#0f172a] h-full min-h-[100vh]
+    py-4
+    px-4
+    md:px-20"
+    >
+      <div className="w-full">
+        <div className="flex flex-row justify-between items-center w-full h-10 px-5 2xl:px-40">
+          <h3 className="cursor-pointer text-3xl font-bold text-cyan-600">
+            Summary!
+          </h3>
+          <a
+            href="https://github.com/SD-softuser"
+            target="_blank"
+            rel="noreferrer">
+            <img src={github} className="w-10 h-10 rounded-lg cursor-pointer" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
